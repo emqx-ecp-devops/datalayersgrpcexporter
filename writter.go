8662158c24050eb3980936d9cdaba6c25ae841c4
Shixiang.W <id1893@outlook.com>
2025-01-15 11:36:30 +0800

    feat: add create table code

--- writter.go
+++ writter.go
@@ -5,6 +5,7 @@ package datalayersexporter
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/emqx-ecp-devops/datalayersexporter/internal/otel2datalayers"
 	"go.opentelemetry.io/collector/component"
@@ -55,6 +56,34 @@ func newDatalayerWritter(logger otel2datalayers.Logger, config *Config, telemetr
 // Start implements component.StartFunc
 func (w *datalayerWritter) Start(ctx context.Context, host component.Host) error {
 	// TODO: to check the database and tables is existed? Create without existing.
+
+	// Creates a database.
+	sql := fmt.Sprintf("create database if not exists %s;", w.db)
+	_, err := w.client.Execute(sql)
+	if err != nil {
+		fmt.Println("Failed to create database: ", err)
+		return err
+	}
+
+	// Creates a table.
+	columsStr := ""
+	if len(w.columns) > 0 {
+		for k, v := range w.columns {
+			columsStr += fmt.Sprintf("`%s` `%s` ,", k, v)
+		}
+	}
+
+	sql = fmt.Sprintf(`
+    create table if not exists %s.%s (
+        ts TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        %s
+        timestamp key(ts)
+    );`, w.db, w.table, columsStr)
+	_, err = w.client.Execute(sql)
+	if err != nil {
+		fmt.Println("Failed to create table: ", err)
+		return err
+	}
 
 	return nil
 }
