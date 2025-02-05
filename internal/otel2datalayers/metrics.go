af2b9fe29b72a7a00ec5e9b1c78b77320255f543
shixiangw <shixiangw@foxmail.com>
2025-02-05 10:33:28 +0000

    feat: add instantce_name feild handle for metric key

--- internal/otel2datalayers/metrics.go
+++ internal/otel2datalayers/metrics.go
@@ -115,7 +115,12 @@ func (w *DatalayerWritter) concatenateSql(metrics MetricsMultipleLines) {
 
 	for _, metric := range metrics {
 		CompareObject.AddColumnsMap(metric.Key, metric.Type)
-		columns += fmt.Sprintf("`%s_%d`,", metric.Key, metric.Type)
+		if metric.Key == "instantce_name" {
+			columns += fmt.Sprintf("`%s`,", metric.Key)
+		} else {
+			columns += fmt.Sprintf("`%s_%d`,", metric.Key, metric.Type)
+		}
+
 		if metric.Type <= int32(pmetric.MetricTypeSummary) {
 			temp := ""
 			if v, ok := metric.Value.(float64); ok {
